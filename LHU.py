a491c25416cc8de48c65d29776e7c203f445c8a1
Mishado <michaladostalova@gmail.com>
2017-03-11 15:17:16 +0100

    Add files via upload

---
+++ LHU.py
@@ -0,0 +1,20 @@
+# Note - this code must run in Python 2.x and you must download
+# http://www.pythonlearn.com/code/BeautifulSoup.py
+# Into the same folder as this program
+
+import urllib
+from BeautifulSoup import *
+
+url = raw_input('Enter - ')
+html = urllib.urlopen(url).read()
+soup = BeautifulSoup(html)
+
+# Retrieve all of the anchor tags
+tags[3] = soup('a')
+count=0
+for tag in tags:
+    
+    print tag.get('href', None)
+    
+    
+    
